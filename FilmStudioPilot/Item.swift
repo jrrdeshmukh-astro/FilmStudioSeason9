c48e5388c3286a64055a6f2978a7b225092c3057
jrrdeshmukh-astro <77753416+jrrdeshmukh-astro@users.noreply.github.com>
2025-12-07 13:10:03 +0530

    Initial commit: FilmStudioPilot with native Apple stack setup

---
+++ FilmStudioPilot/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FilmStudioPilot
+//
+//  Created by Jai Deshmukh on 12/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
